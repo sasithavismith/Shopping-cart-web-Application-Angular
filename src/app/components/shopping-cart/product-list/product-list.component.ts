9acdbea22985c58733f08141ef56f7c8103451c1
saitha vismith <sasithavismith@gmail.com>
2020-05-31 21:04:47 +0530

    third commit

--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
+
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  productList: Product[] =[]
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productList= this.productService.getProduct()
   }
 
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+products:Product[]=[
+new Product(1,'Product 1', 'This is the product 1 description. The product is really kool!', 100),
+new Product(2,'Product 2', 'This is the product 2 description. The product is really kool!', 150),
+new Product(3,'Product 3', 'This is the product 3 description. The product is really kool!', 50),
+new Product(4,'Product 4', 'This is the product 4 description. The product is really kool!', 200),
+new Product(5,'Product 5', 'This is the product 5 description. The product is really kool!', 100),
+new Product(6,'Product 6', 'This is the product 6 description. The product is really kool!', 150),
+new Product(7,'Product 7', 'This is the product 7 description. The product is really kool!', 250)
+]
+
+
+  constructor() { }
+  getProduct(): Product[]{
+    return this.products
+  }
+}
